e9d9b3a97e5d4bca7247326f0aab69763ab044f2
Arsh150701 <45517849+Arsh150701@users.noreply.github.com>
2021-04-25 01:35:54 +0530

    Facebook login implemented and main activity updated

--- app/src/main/java/com/example/fliprhackathon/MainActivity.java
+++ app/src/main/java/com/example/fliprhackathon/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.fliprhackathon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private TextView info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        info = findViewById(R.id.info);
+        info.setText("Welcome " + currentUser.getDisplayName());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    @Override
+    public void onBackPressed() {
+//        super.onBackPressed();
+        finish();
     }
 }
